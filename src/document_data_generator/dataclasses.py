5e1b60216ac00a9f00be0c7371fe7da08ca8b709
Semyon Dipner <semyondipner>
2024-12-04 00:41:20 +0300

    Send a commit

--- src/document_data_generator/dataclasses.py
+++ src/document_data_generator/dataclasses.py
@@ -1,14 +1,26 @@
+""" Dataclasses for Document Data after generation"""
 from dataclasses import dataclass
+
+from src.data_generator import (
+    InvoiceCompanyData,
+    InvoiceDateTimeData,
+    InvoiceFinancialData,
+    InvoiceItemData,
+)
 
 
 @dataclass
 class Entity:
+    """Structure of each Entity"""
+
     value: str
     bboxes: list[int | float]
 
 
 @dataclass
 class SnilsData:
+    """Snils Data Structure"""
+
     snils_number: Entity
     family_name: Entity
     middle_name: Entity
@@ -18,3 +30,14 @@ class SnilsData:
     city: Entity
     region: Entity
     gender: Entity
+
+
+@dataclass
+class InvoiceData:
+    """Invoice Data Structure"""
+
+    saler: InvoiceCompanyData
+    buyer: InvoiceCompanyData
+    items: list[InvoiceItemData]
+    datatime: InvoiceDateTimeData
+    financial: InvoiceFinancialData
