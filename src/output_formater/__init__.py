286d1b0d453129de75fbd456a02ec5f51d1ad29b
starminalush <alica154323@gmail.com>
2024-12-11 02:33:20 +0500

    update readme. add instruction for adding new document. add pipeline

--- src/output_formater/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.output_formater.json_output_formater import JSONOutputFormater
-
-output_formaters = {"json": JSONOutputFormater()}
-
-
-def get_output_formater(name):
-    return output_formaters.get(name)
