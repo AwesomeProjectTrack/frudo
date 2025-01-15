2c183a06a49070ba8e54eed753788a4301758687
starminalush <103132748+starminalush@users.noreply.github.com>
2025-01-16 02:37:59 +0500

    Revert "add passport hd"

--- src/document_data_generator/dataclasses.py
+++ src/document_data_generator/dataclasses.py
@@ -36,8 +36,6 @@ class SnilsData:
 class PassportData:
     pser: Entity
     pnum: Entity
-    pser2: Entity  # для картинки нужны разные поля (bbox)
-    pnum2: Entity  # для разных страниц
     vyd1: Entity
     vyd2: Entity
     vyd3: Entity
@@ -53,29 +51,25 @@ class PassportData:
     bpl2: Entity
     bpl3: Entity
 
-    keys_descr = {
-        "pser": "Серия паспорта",
-        "pnum": "Номер паспорта",
-        "vyd1": "Паспорт выдан (1 строка)",
-        "vyd2": "Паспорт выдан (2 строка)",
-        "vyd3": "Паспорт выдан (3 строка)",
-        "dvyd": "Дата выдачи",
-        "npod": "Номер подразделения",
-        "fam1": "Фамилия (1 строка)",
-        "fam2": "Фамилия (2 строка)",
-        "name": "Имя",
-        "fnam": "Отчество",
-        "bdat": "Дата рождения",
-        "sx": "Пол",
-        "bpl1": "Место рождения (1 строка)",
-        "bpl2": "Место рождения (2 строка)",
-        "bpl3": "Место рождения (3 строка)",
-    }
 
-    def set_bboxes(self, key: Entity, bbx: list):
-        self.key["bboxes"] = bbx
-
-
+"""
+    ['PSER', 'Серия паспорта'],
+    ['PNUM', 'Номер паспорта'],
+    ['VYD1', 'Паспорт выдан (1 строка)'],
+    ['VYD2', 'Паспорт выдан (2 строка)'],
+    ['VYD3', 'Паспорт выдан (3 строка)'],
+    ['DVYD', 'Дата выдачи'],
+    ['NPOD', 'Номер подразделения'],
+    ['FAM1', 'Фамилия (1 строка)'],
+    ['FAM2', 'Фамилия (2 строка)'],
+    ['NAME', 'Имя'],
+    ['FNAM', 'Отчество'],
+    ['BDAT', 'Дата рождения'],
+    ['SX',   'Пол'],
+    ['BPL1', 'Место рождения (1 строка)'],
+    ['BPL2', 'Место рождения (2 строка)'],
+    ['BPL3', 'Место рождения (3 строка)']
+"""
 class InvoiceData:
     """Invoice Data Structure"""
 
@@ -84,3 +78,37 @@ class InvoiceData:
     items: list[InvoiceItemData]
     datatime: InvoiceDateTimeData
     financial: InvoiceFinancialData
+
+
+@dataclass
+class NewTinsData:
+    """Tins Data Structure for new version of tins"""
+
+    tins_number: Entity
+    family_name: Entity
+    middle_name: Entity
+    first_name: Entity
+    name: Entity
+    birth_date: Entity
+    reg_date: Entity
+    city: Entity
+    region: Entity
+    gender: Entity
+    full_place: Entity
+
+
+@dataclass
+class OldTinsData:
+    """Tins Data Structure for old version of tins"""
+
+    tins_number: Entity
+    family_name: Entity
+    middle_name: Entity
+    first_name: Entity
+    name: Entity
+    birth_date: Entity
+    reg_date: Entity
+    city: Entity
+    region: Entity
+    gender: Entity
+    full_place: Entity
---
+++ src/document_data_generator/tins_new.py
@@ -0,0 +1,94 @@
+import json
+import locale
+from pathlib import Path
+
+from src.data_generator import (
+    BboxTins,
+    DateGenerator,
+    FullNameGenerator,
+    Gender,
+    GeoPlace,
+    StartCoords,
+    TinsNumber,
+)
+from src.document_data_generator.base_document_data_generator import (
+    BaseDocumentDataGenerator,
+)
+from src.document_data_generator.dataclasses import Entity, NewTinsData
+
+
+class NewTinsDocumentDataGenerator(BaseDocumentDataGenerator):
+    @staticmethod
+    def generate() -> NewTinsData:
+        locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+        gender = Gender.generate()
+        name = FullNameGenerator.generate(gender)
+        full_name = name.last_name + " " + name.first_name + " " + name.middle_name
+        place = GeoPlace.generate()
+        full_place = place.region + " " + place.city
+        birth_date = DateGenerator.generate()
+        birth_date = birth_date.strftime("%d.%m.%Y года")
+        registration_date = DateGenerator.generate()
+        registration_date = registration_date.strftime("%d.%m.%Y года")
+        tins_number = TinsNumber.generate()
+
+        annotations_path = Path("src/templates/tins/data.json")
+
+        with open(annotations_path) as file:
+            anno = json.load(file)
+
+        start_coords_new = StartCoords.generate(width=anno["new"]["width"], height=anno["new"]["heigth"])
+
+        anno["new"]["name"]["value"] = full_name
+        anno["new"]["name"]["x"] = start_coords_new["name"][0]
+        anno["new"]["name"]["y"] = start_coords_new["name"][1]
+        anno["new"]["name"]["bbox"] = BboxTins().generate(
+            text=full_name, old_coords=[anno["new"]["name"]["x"], anno["new"]["name"]["y"]]
+        )
+        anno["new"]["birth_date"]["value"] = birth_date
+        anno["new"]["birth_date"]["x"] = start_coords_new["birth_date"][0]
+        anno["new"]["birth_date"]["y"] = start_coords_new["birth_date"][1]
+        anno["new"]["birth_date"]["bbox"] = BboxTins().generate(
+            text=birth_date, old_coords=[anno["new"]["birth_date"]["x"], anno["new"]["birth_date"]["y"]]
+        )
+
+        anno["new"]["sex"]["value"] = gender
+        anno["new"]["sex"]["x"] = start_coords_new["sex"][0]
+        anno["new"]["sex"]["y"] = start_coords_new["sex"][1]
+        anno["new"]["sex"]["bbox"] = BboxTins().generate(
+            text=gender, old_coords=[anno["new"]["sex"]["x"], anno["new"]["sex"]["y"]]
+        )
+
+        anno["new"]["place_of_birth"]["value"] = full_place
+        anno["new"]["place_of_birth"]["x"] = start_coords_new["place_of_birth"][0]
+        anno["new"]["place_of_birth"]["y"] = start_coords_new["place_of_birth"][1]
+        anno["new"]["place_of_birth"]["bbox"] = BboxTins().generate(
+            text=full_place, old_coords=[anno["new"]["place_of_birth"]["x"], anno["new"]["place_of_birth"]["y"]]
+        )
+
+        anno["new"]["issued"]["value"] = registration_date
+        anno["new"]["issued"]["x"] = start_coords_new["issued"][0]
+        anno["new"]["issued"]["y"] = start_coords_new["issued"][1]
+        anno["new"]["issued"]["bbox"] = BboxTins().generate(
+            text=registration_date, old_coords=[anno["new"]["issued"]["x"], anno["new"]["issued"]["y"]]
+        )
+
+        anno["new"]["tin"]["value"] = tins_number
+        anno["new"]["tin"]["x"] = start_coords_new["tin"][0]
+        anno["new"]["tin"]["y"] = start_coords_new["tin"][1]
+        anno["new"]["tin"]["bbox"] = BboxTins().generate(
+            text=tins_number, old_coords=[anno["new"]["tin"]["x"], anno["new"]["tin"]["y"]]
+        )
+        return NewTinsData(
+            tins_number=Entity(value=tins_number, bboxes=anno["new"]["tin"]["bbox"]),
+            family_name=Entity(value=name.last_name, bboxes=anno["new"]["name"]["bbox"]),
+            middle_name=Entity(value=name.middle_name, bboxes=anno["new"]["name"]["bbox"]),
+            first_name=Entity(value=name.first_name, bboxes=anno["new"]["name"]["bbox"]),
+            name=Entity(value=full_name, bboxes=anno["new"]["name"]["bbox"]),
+            birth_date=Entity(value=birth_date, bboxes=anno["new"]["birth_date"]["bbox"]),
+            reg_date=Entity(value=registration_date, bboxes=anno["new"]["issued"]["bbox"]),
+            city=Entity(value=place, bboxes=anno["new"]["place_of_birth"]["bbox"]),
+            region=Entity(place.region, bboxes=anno["new"]["place_of_birth"]["bbox"]),
+            full_place=Entity(value=full_place, bboxes=anno["new"]["place_of_birth"]["bbox"]),
+            gender=Entity(value=gender, bboxes=anno["new"]["sex"]["bbox"]),
+        )
